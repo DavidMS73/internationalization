7e464f43302eec5db03e4ca2620eaa42a4fa0892
German <gd.martinez@uniandes.edu.co>
2020-11-12 02:02:38 -0500

    6 retos

--- src/components/jobsList.js
+++ src/components/jobsList.js
@@ -1,53 +1,76 @@
-import React from 'react';
+import React from "react";
+import { FormattedMessage } from "react-intl";
 import Job from "./job";
 
 export default class JobsList extends React.Component {
-
-  state = { 
-  	"offers": [
-    	{
-    	  "id": "0001",
-        "name": "Manager",
-    	  "company": "Schneider Electric", 
-    	  "salary": 4.5,
-    	  "city": "Bogotá, Colombia",
-        "date": "2019-03-26"
-      }, 
+  state = {
+    offers: [
       {
-        "id": "0002",
-        "name": "Software Engineer",
-        "company": "Google Inc.", 
-        "salary": 20,
-        "city": "Palo Alto, CA, USA",
-        "date": "2019-03-27"
+        id: "0001",
+        name: "Manager",
+        company: "Schneider Electric",
+        salary: 4.5,
+        city: "Bogotá, Colombia",
+        date: "2019-03-26",
+        visit: 3345666,
       },
       {
-        "id": "0003",
-        "name": "Nurse",
-        "company": "Clínica La Aurora", 
-        "salary": 1,
-        "city": "Cali, Colombia",
-        "date": "2019-03-28"
-      }
-    ]
+        id: "0002",
+        name: "Software Engineer",
+        company: "Google Inc.",
+        salary: 20,
+        city: "Palo Alto, CA, USA",
+        date: "2019-03-27",
+        visit: 2456543999,
+      },
+      {
+        id: "0003",
+        name: "Nurse",
+        company: "Clínica La Aurora",
+        salary: 1,
+        city: "Cali, Colombia",
+        date: "2019-03-28",
+        visit: 387654345678,
+      },
+    ],
   };
 
   render() {
     return (
       <div>
         <table className="table">
-          <thead className="thead-dark">
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Position</th>
-              <th scope="col">Company</th>
-              <th scope="col">Salary</th>
-              <th scope="col">City</th>
-              <th scope="col">Publication date</th>
-            </tr>
-          </thead>
+          <FormattedMessage id="tableHead">
+            {(txt) => (
+              <thead className={txt}>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">
+                    <FormattedMessage id="Position" />
+                  </th>
+                  <th scope="col">
+                    <FormattedMessage id="Company" />
+                  </th>
+                  <th scope="col">
+                    <FormattedMessage id="Salary" />
+                  </th>
+                  <th scope="col">
+                    <FormattedMessage id="City" />
+                  </th>
+                  <th scope="col">
+                    <FormattedMessage id="PublicationDate" />
+                  </th>
+                  <th scope="col">
+                    <FormattedMessage id="Views" />
+                  </th>
+                </tr>
+              </thead>
+            )}
+          </FormattedMessage>
+
           <tbody>
-              {this.state.offers.map( (e,i) => <Job key={i} offer={e}/>)}
+            {this.state.offers.map((e, i) => (
+              <Job key={i} offer={e} />
+            ))}
           </tbody>
         </table>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from "react-intl";
+import messages_es from "./locales/es";
+import messages_en from "./locales/en";
 
 import JobsList from "./components/jobsList";
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+const messages = {
+  es: messages_es,
+  en: messages_en,
+};
+
+const language = navigator.language.split(/[-_]/)[0];
+
+ReactDOM.render(
+  <IntlProvider locale={navigator.language} messages={messages[language]}>
+    <JobsList />
+  </IntlProvider>,
+  document.getElementById("root")
+);
